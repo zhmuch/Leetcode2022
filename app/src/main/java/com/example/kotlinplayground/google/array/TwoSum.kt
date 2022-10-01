0550a6a1fc38f1fb20425141111e1789dffa7182
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-01 15:30:37 -0700

    001 Two Sum

---
+++ app/src/main/java/com/example/kotlinplayground/google/array/TwoSum.kt
@@ -0,0 +1,40 @@
+package com.example.kotlinplayground.google.array
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    return loopFind(nums, target)
+}
+
+/**
+ * Naive loop find
+ */
+fun loopFind(nums: IntArray, target: Int): IntArray {
+    for (i in nums.indices) {
+        for (j in (i + 1) until nums.size) {
+            val sum = nums[i] + nums[j]
+            if (sum == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    // Invalid return value
+    return intArrayOf(-1, -1)
+}
+
+/**
+ * Use more space to memorize result
+ */
+fun hashtableFind(nums: IntArray, target: Int): IntArray {
+    val records = HashMap<Int, Int>()
+    for (i in nums.indices) {
+        val remain = target - nums[i]
+        records[remain].also { remainIndex ->
+            if (remainIndex != null) {
+                return intArrayOf(remainIndex, i)
+            } else {
+                records[nums[i]] = i
+            }
+        }
+    }
+    // Invalid return value
+    return intArrayOf(-1, -1)
+}
