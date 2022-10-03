6acf6dc423ad0c7ced9d3170d4fccf7de3b26a4e
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-02 22:21:48 -0700

    253 Meeting Rooms II

--- app/src/main/java/com/example/kotlinplayground/google/array/MeetingRooms.kt
+++ app/src/main/java/com/example/kotlinplayground/google/array/MeetingRooms.kt
@@ -2,6 +2,7 @@ package com.example.kotlinplayground.google.array
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.util.LinkedList
 import java.util.PriorityQueue
 
 /**
@@ -55,4 +56,96 @@ class MeetingRooms {
         val startTime: Int,
         val endTime: Int
     )
+}
+
+/**
+ * 253 Meeting Rooms II
+ * https://leetcode.com/problems/meeting-rooms-ii/
+ */
+class MeetingRoomsII {
+
+    /**
+     * Solution 1:
+     *
+     * Create an array contains all time unit, fill time unit with each intervals
+     * keep track of maximum room needed at same time
+     */
+
+    /**
+     * Solution 2:
+     *
+     * 1. Put the element in the priority heap with custom comparator
+     * 2. Put all intervals into the heap
+     * 3. Pop up intervals one by one
+     * 4. keep track of maximum room needed at same time
+     */
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun minMeetingRooms(intervals: Array<IntArray>): Int {
+        val intervalsPool = PriorityQueue<Interval> { interval1, interval2 ->
+            interval1.startTime - interval2.startTime
+        }
+
+        // Construct priority heap for intervals
+        for (interval in intervals) {
+            val newInterval = Interval(interval[0], interval[1])
+            intervalsPool.add(newInterval)
+        }
+
+        var maxNumberOfRooms = 0
+        var inUseRoomEndTimes: MutableList<Int> = LinkedList<Int>()
+        // Pop up all intervals
+        while (intervalsPool.isNotEmpty()) {
+            val currentInterval = intervalsPool.poll()
+            // Remove expired rooms
+            inUseRoomEndTimes = inUseRoomEndTimes.removeExpiredEndTimes(currentInterval.startTime)
+            // Add a new room for current one
+            inUseRoomEndTimes.add(currentInterval.endTime)
+            maxNumberOfRooms = Math.max(maxNumberOfRooms, inUseRoomEndTimes.size)
+
+        }
+        return maxNumberOfRooms
+    }
+
+    private fun MutableList<Int>.removeExpiredEndTimes(targetTime: Int): MutableList<Int> {
+        return this.filter { it > targetTime } as MutableList<Int>
+    }
+
+    data class Interval(
+        val startTime: Int,
+        val endTime: Int
+    )
+
+    /**
+     * Solution 3:
+     *
+     * 1. Ignore the bond of a start time and end time,
+     * 2. Only check how many "room event" is ongoing
+     */
+    fun minMeetingRoomsII(intervals: Array<IntArray>): Int {
+        val intervalsSize = intervals.size
+        val startTimes = Array(intervalsSize) { -1 }
+        val endTimes = Array(intervalsSize) { -1 }
+
+        // Construct start and end times array
+        for (i in 0 until intervalsSize) {
+            startTimes[i] = intervals[i][0]
+            endTimes[i] = intervals[i][1]
+        }
+
+        // Sort two arrays
+        startTimes.sort()
+        endTimes.sort()
+
+        var maxRoom = 0
+        var endTimeIndex = 0
+        //
+        for (i in 0 until intervalsSize) {
+            if (startTimes[i] < endTimes[endTimeIndex]) {
+                maxRoom++
+            } else {
+                endTimeIndex++
+            }
+        }
+        return maxRoom
+    }
 }
