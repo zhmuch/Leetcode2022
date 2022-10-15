1dd0b6e98bae08cd22ed41dfe3acb9a8c039bd94
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-15 02:01:40 -0700

    130. Surrounded Regions

---
+++ app/src/main/java/com/example/kotlinplayground/google/SurroundedRegions.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 130. Surrounded Regions
+ *
+ * https://leetcode.com/problems/surrounded-regions/
+ */
+class SurroundedRegions {
+
+    /**
+     * Solution 1:
+     *
+     * Brute force, check node for four direction
+     */
+
+    /**
+     * Solution 2:
+     *
+     * BFS,
+     * flip the node in place
+     */
+    fun solve(board: Array<CharArray>): Unit {
+
+    }
+}
