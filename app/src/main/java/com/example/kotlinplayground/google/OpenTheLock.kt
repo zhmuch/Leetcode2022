e161687e34020d57c7f1204f87d248e2ec54185e
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-15 02:05:32 -0700

    752. Open the Lock

---
+++ app/src/main/java/com/example/kotlinplayground/google/OpenTheLock.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 752. Open the Lock
+ *
+ * https://leetcode.com/problems/open-the-lock/
+ */
+class OpenTheLock {
+
+    /**
+     * Solution:
+     *
+     * BFS
+     *
+     * bi-direction BFS for optimization
+     */
+    fun openLock(deadends: Array<String>, target: String): Int {
+
+    }
+}
