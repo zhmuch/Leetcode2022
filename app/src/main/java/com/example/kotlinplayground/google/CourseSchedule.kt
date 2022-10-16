0b82076ea796629417c7e3b5f9ccd0428a6ba5f0
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-16 01:31:50 -0700

    207. Course Schedule & 210. Course Schedule II

---
+++ app/src/main/java/com/example/kotlinplayground/google/CourseSchedule.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 207. Course Schedule
+ *
+ * https://leetcode.com/problems/course-schedule/
+ */
+class CourseSchedule {
+
+    /**
+     * Solution 1:
+     *
+     * DFS + topological sort
+     */
+    fun canFinish(numCourses: Int, prerequisites: Array<IntArray>): Boolean {
+
+    }
+
+    /**
+     * Solution 2:
+     *
+     * Backtracking:
+     * This problem is equivalent to finding if a cycle exists in a directed graph.
+     * If a cycle exists, no topological ordering exists and therefore it will be impossible
+     * to take all courses.
+     */
+
+    /**
+     * Solution 3:
+     *
+     * Optimization based on solution 2
+     */
+}
---
+++ app/src/main/java/com/example/kotlinplayground/google/CourseScheduleII.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 210. Course Schedule II
+ *
+ * https://leetcode.com/problems/course-schedule-ii/
+ */
+class CourseScheduleII {
+
+    /**
+     * Solution 1
+     *
+     * Similar to 207. Course Schedule
+     * return the topological sort of the DAG
+     */
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+
+    }
+
+    /**
+     * Solution 2:
+     *
+     * Using Node Indegree
+     *
+     * 
+     */
+}
