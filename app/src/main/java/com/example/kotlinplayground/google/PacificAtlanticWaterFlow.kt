64df10d585c90bda4c464334dff94c1ff82dd7ed
Muchen Zhang <mzhang@muchensmbpm1max.lan>
2022-10-16 00:13:15 -0700

    1091. Shortest Path in Binary Matrix & 417. Pacific Atlantic Water Flow

---
+++ app/src/main/java/com/example/kotlinplayground/google/PacificAtlanticWaterFlow.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 417. Pacific Atlantic Water Flow
+ *
+ * https://leetcode.com/problems/pacific-atlantic-water-flow/
+ */
+class PacificAtlanticWaterFlow {
+
+    /**
+     * Solution 1:
+     *
+     * BFS + Union Find
+     */
+    fun pacificAtlantic(heights: Array<IntArray>): List<List<Int>> {
+
+    }
+}
---
+++ app/src/main/java/com/example/kotlinplayground/google/ShortestPathInBinaryMatrix.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinplayground.google
+
+/**
+ * 1091. Shortest Path in Binary Matrix
+ *
+ * https://leetcode.com/problems/shortest-path-in-binary-matrix/
+ */
+class ShortestPathInBinaryMatrix {
+
+    /**
+     * Solution 1
+     *
+     * BFS
+     *
+     * bi-direction for optimization
+     */
+    fun shortestPathBinaryMatrix(grid: Array<IntArray>): Int {
+
+    }
+
+    /**
+     * Solution 2
+     *
+     * A*
+     * ???
+     *
+     * A* search algorithm
+     *
+     * https://en.wikipedia.org/wiki/A*_search_algorithm
+     */
+}
